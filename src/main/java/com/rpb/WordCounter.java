882493d90829027a5043b2af2cfbed1619e92203
rpb2 <remi.pece-bourgerolle@gatech.edu>
2018-02-07 22:17:21 +0100

    [clean] Refactor code, include javadoc

--- src/main/java/com/rpb/WordCounter.java
+++ src/main/java/com/rpb/WordCounter.java
@@ -6,23 +6,42 @@ import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
+
+/*
+ * 
+ * The Apache PDFBox® library is an open source Java tool for working with PDF documents. 
+ * It allows creation of new PDF documents, manipulation of existing documents and the 
+ * 	ability to extract content from documents.
+ * 
+ * Apache PDFBox is published under the Apache License v2.0.
+ * 
+ * Apache PDFBox® is available here :
+ * https://pdfbox.apache.org/
+ * 
+ */
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
 
+/**
+ * 
+ * @author rpb
+ *
+ */
 public class WordCounter {
 	
-	public static int countWords(String fileName) {
+	public static int countWords(String filePath) {
 		PDDocument document = null;
 		try {
-			document = PDDocument.load(new File(fileName));
+			document = PDDocument.load(new File(filePath));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		
-		/*
-		 * Check if file is encrypted
-		 */
-		if(null == document || document.isEncrypted()) {
+		if(null == document) {
+			System.err.println("The specified file could not be opened");
+			return 0;
+		}
+		if(document.isEncrypted()) {
 			System.err.println("Encrypted files are not supported");
 			return 0;
 		}
@@ -35,39 +54,44 @@ public class WordCounter {
 		}
 	}
 	
+	/**
+	 * 
+	 * @param document
+	 * @return the number of words in document
+	 * @throws IOException
+	 */
 	public static int countWords(PDDocument document) throws IOException {
 		PDFTextStripper stripper = new PDFTextStripper();
 		String s = stripper.getText(document);
-		return countStringWords(s);
+		return countWordsInString(s);
 	}
 	
-	public static int countStringWords(String originalText) {
-		String trim = originalText.trim();
-		String asciiText = trim.replaceAll("[^\\x00-\\x7F]", "");
+	/**
+	 * 
+	 * @param text
+	 * @return the number of words in text
+	 */
+	public static int countWordsInString(String text) {
+		String trimmedText = text.trim();
+		/*
+		 * Suppress non-ASCII characters
+		 */
+		String asciiText = trimmedText.replaceAll("[^\\x00-\\x7F]", "");
 		if ( asciiText.isEmpty() ) {
 		    return 0;
 		}
-		/*
-		 * FOR DEBUG PURPOSE ONLY
-		 * System.out.println(asciiText);
-		 */
 		return asciiText.split("\\s+").length;
 	}
 	
 	public static void main(String[] args) {
 		JFileChooser chooser = new JFileChooser();
-        FileNameExtensionFilter filter = new FileNameExtensionFilter(
-                "PDF Documents", "pdf");
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("PDF Documents", "pdf");
         chooser.setFileFilter(filter);
         int returnVal = chooser.showOpenDialog(null);
         if(returnVal == JFileChooser.APPROVE_OPTION) {
         	String fileName = chooser.getSelectedFile().getPath();
         	String message = fileName + " contains " + countWords(fileName) + " words.";
         	JOptionPane.showMessageDialog(null, message);
-        	System.out.println("");
-        	System.out.println("-------------------------------------------------------");
-        	System.out.println(message);
-        	System.out.println("-------------------------------------------------------");
         }
 	}
 }
